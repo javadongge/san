bd8b2a905f6a88a462ae9b9627167df0e1abe66e
javadongge <javadongge>
2018-12-03 17:51:54 +0800

    第一次commit

---
+++ src/main/java/com/zhao/dong/DongApplication.java
@@ -0,0 +1,12 @@
+package com.zhao.dong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DongApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DongApplication.class, args);
+    }
+}
---
+++ src/test/java/com/zhao/dong/DongApplicationTests.java
@@ -0,0 +1,16 @@
+package com.zhao.dong;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DongApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
